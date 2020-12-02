d5975769347714227abdaf1e5fd97160f1ba2ba3
Raakka <jrip03@gmail.com>
2020-12-01 22:29:46 -0500

    fixing the appid match

--- src/handlers.rs
+++ src/handlers.rs
@@ -2,31 +2,41 @@
 mod models;
 
 pub mod px_generators{
-    
-    use actix_web::{ 
+
+    use actix_web::{
         web,
         Error,
         HttpResponse
     };
-   
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// DYNAMIC PX2 GEN FUNCTION
+
     pub async fn genpx2 (
         client_req: web::Json<Px2ClientReq>,
     ) -> Result<HttpResponse, Error> {
-        // this is just testing
         let client_req_inf: Px2ClientReq = client_req.into_inner();
-        
-        // okay are we doing things now?
-        // this is mad invalid rn 
-        // TODO(XVI): fix this bs bro??!!
-        let px2_result: web::Json<Px2ClientRes> = match client_req_inf.site {
-            // add more sites here
-            "walmart" => ok(),
-            "hibbet" => ok(),
-            "solebox" => ok(),
-        }
 
-        Ok(HttpResponse::Ok().json(px2_result.into_inner()))
+        // DYNAMIC PXAPPID!
+        // so this sets the px appid based on the site in the req
+        if !client_req_inf.site {
+            Ok(HttpResponse::BadRequest())
+        } else {
+            let px2_appid: String = match client_req_inf.site {
+                // add more sites here
+                "walmart" => "PXu6b0qd2S",
+                "hibbet" => "PXAJDckzHD",
+                "solebox" => "PXuR63h57Z",
+                _ =>(),
+            };
+
+            //just responding to request with the px appid for now
+            Ok(HttpResponse::Ok().json(px2_appid))
+        }
     }
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// DYNAMIC PX3 GEN FUNCTION
 
     pub async fn genpx3 (
         client_req: web::Json<Px3ClientReq>,
@@ -34,5 +44,5 @@ pub mod px_generators{
         // same tesing junk...
         let client_req_inf: Px3ClientReq = client_req.into_inner();
         Ok(HttpResponse::Ok().json(client_req_inf))
-    } 
+    }
 }
---
+++ src/px_gen_functions.rs
@@ -0,0 +1,26 @@
+// TODO(XVI): Please try to keep up with version changes and such
+mod px_gen_functions{
+    /// START PX GENERATOR
+    /// PX2 @ v6.0.2
+    pub fn generatepx2_602() {
+
+    }
+
+    /// START PX GENERATOR
+    /// PX3 @ v6.0.2
+    pub fn generatepx3_602() {
+
+    }
+
+    /// START PX GENERATOR
+    /// PX2 @ v6.2.1
+    pub fn generatepx2_621() {
+
+    }
+
+    /// START PX GENERATOR
+    /// PX3 @ v6.2.1
+    pub fn generatepx3_621() {
+
+    }
+}
